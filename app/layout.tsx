0013c9bdf4ab132635251744b3622962ccc00e5e
Seamus Lowry <spl1owry3@gmail.com>
2024-12-23 15:37:38 +0000

    chore: title and description metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,18 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import { parse } from 'csv-parse';
 import { geistMono, geistSans } from './fonts';
 import { readFile } from 'fs/promises';
 
 export async function generateMetadata(): Promise<Metadata> {
-  const csv = await readFile('./public/metadata/default.csv', 'utf8');
-
-  const metadata = await parse(csv, {
-    columns: true,
-    skip_empty_lines: true,
-    // TODO: something other than toArray?
-  }).toArray();
+  const title = await readFile('./public/metadata/default/title.txt', 'utf8');
+  const description = await readFile(
+    './public/metadata/default/description.txt',
+    'utf8',
+  );
 
   return {
-    title: metadata[0].title,
-    description: metadata[0].description,
+    title,
+    description,
   };
 }
 
