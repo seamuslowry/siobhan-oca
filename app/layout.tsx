0e4ad2361547b4015756d83cb39e2e6ed78cc95f
Seamus Lowry <spl1owry3@gmail.com>
2024-12-23 17:46:48 +0000

    feat: desktop nav links

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,28 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { geistMono, geistSans } from '../utils/fonts';
 import { retrieveMetadata } from '@/utils/metadata';
+import ButtonLink from '@/components/ButtonLink';
 
 export async function generateMetadata(): Promise<Metadata> {
-  console.log(await retrieveMetadata('default'));
-
   return retrieveMetadata('default');
+}
+
+function Navbar() {
+  return (
+    <nav className="bg-sky-700">
+      <div className="container mx-auto flex items-center justify-between p-2">
+        <div className="flex text-lg font-bold rounded">
+          <ButtonLink href="/">Siobhan Oca</ButtonLink>
+        </div>
+        <div id="desktop-nav-links" className="hidden md:flex space-x-3">
+          <ButtonLink href="/">Research</ButtonLink>
+          <ButtonLink href="/">Teaching</ButtonLink>
+          <ButtonLink href="/">News</ButtonLink>
+          <ButtonLink href="/">Who</ButtonLink>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default function RootLayout({
@@ -17,8 +34,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.className} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
