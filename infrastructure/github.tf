82819f30b9c3142bbc0160a7c24da6924fc1fa05
Seamus Lowry <spl1owry3@gmail.com>
2024-12-24 10:28:45 +0000

    fix: dependabot secret

--- infrastructure/github.tf
+++ infrastructure/github.tf
@@ -3,3 +3,9 @@ resource "github_actions_secret" "webapp_api_token" {
   secret_name     = "AZURE_STATIC_WEB_APPS_API_TOKEN"
   plaintext_value = azurerm_static_web_app.siobhanoca.api_key
 }
+
+resource "github_dependabot_secret" "webapp_api_token" {
+  repository      = "siobhan-oca"
+  secret_name     = "AZURE_STATIC_WEB_APPS_API_TOKEN"
+  plaintext_value = azurerm_static_web_app.siobhanoca.api_key
+}
