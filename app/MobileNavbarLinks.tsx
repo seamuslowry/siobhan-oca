bdb4e8151a06fdcd4fa68f98a251acfc89aeaa59
Seamus Lowry <spl1owry3@gmail.com>
2024-12-25 11:31:08 -0500

    feat: kinda working sidebar

---
+++ app/MobileNavbarLinks.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import ButtonLink from '@/components/ButtonLink';
+import clsx from 'clsx';
+import { useCallback, useState } from 'react';
+
+export function MobileNavbarLinks() {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = useCallback(e => {
+    console.log(e);
+  }, []);
+
+  return (
+    <div>
+      <button onClick={() => setOpen(true)}>Open</button>
+      <dialog
+        open={open}
+        onClose={() => setOpen(false)}
+        onClick={handleClick}
+        className="fixed top-0 left-full m-0 p-0 w-full max-w-xs h-screen bg-gray-800 text-white shadow-lg flex flex-col transform transition-transform open:-translate-x-full"
+      >
+        <ButtonLink href="/">Research</ButtonLink>
+        <ButtonLink href="/">Teaching</ButtonLink>
+        <ButtonLink href="/">News</ButtonLink>
+        <ButtonLink href="/">Who</ButtonLink>
+      </dialog>
+    </div>
+  );
+}
