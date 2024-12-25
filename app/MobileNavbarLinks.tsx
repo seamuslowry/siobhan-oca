02d84c97f46dec3fb16a67dcab1bf813e1252a8c
Seamus Lowry <spl1owry3@gmail.com>
2024-12-25 11:34:47 -0500

    feat: small cleanup

--- app/MobileNavbarLinks.tsx
+++ app/MobileNavbarLinks.tsx
@@ -1,14 +1,10 @@
 'use client';
 
 import ButtonLink from '@/components/ButtonLink';
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 
 export function MobileNavbarLinks() {
   const [open, setOpen] = useState(false);
-
-  const handleClick = useCallback(e => {
-    console.log(e);
-  }, []);
 
   return (
     <div>
@@ -16,7 +12,6 @@ export function MobileNavbarLinks() {
       <dialog
         open={open}
         onClose={() => setOpen(false)}
-        onClick={handleClick}
         className="fixed top-0 left-full m-0 p-0 w-full max-w-xs h-screen bg-gray-800 text-white shadow-lg flex flex-col z-50 transform transition-transform open:-translate-x-full"
       >
         <ButtonLink href="/">Research</ButtonLink>
