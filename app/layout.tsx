44e59bd425b9693f1c7577f96bf9f8f4ed49c725
Seamus Lowry <spl1owry3@gmail.com>
2024-12-25 21:45:50 -0500

    fix: hide overflow when dialog open

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.className} ${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.className} ${geistSans.variable} ${geistMono.variable} antialiased [&:has(dialog[open])]:overflow-hidden`}
       >
         <Navbar />
         {children}
