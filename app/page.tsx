841d6d9d0364c72a4b84e511ddc9778a0abf6788
Seamus Lowry <spl1owry3@gmail.com>
2025-01-01 12:15:48 -0500

    feat: banner image and email

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import heroImage from '@/assets/home/hero.png';
+import bannerImage from '@/assets/home/banner.png';
 
 export default function Home() {
   return (
@@ -12,7 +13,21 @@ export default function Home() {
           placeholder="blur"
           unoptimized
         />
-        <div className="bg-sky-700/30 h-[80vh] mx-auto w-10/12 sm:absolute sm:ml-[10%] sm:w-[30vw] sm:min-w-80" />
+        <div className="bg-sky-700/30 grid grid-rows-[1fr_min-content] gap-3 py-8 px-4 h-[80vh] mx-auto w-10/12 sm:absolute sm:ml-[10%] sm:w-[30vw] sm:min-w-80">
+          <Image
+            src={bannerImage}
+            alt="Banner image description"
+            placeholder="blur"
+            className="object-contain h-full"
+            unoptimized
+          />
+          <p className="text-center text-xl">
+            Email:{' '}
+            <a href="mailto:skr23@duke.edu" className="underline">
+              skr23@duke.edu
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
