e1bfd09f58e8c00deb92d901ed5928e259e38652
Seamus Lowry <spl1owry3@gmail.com>
2025-01-01 11:19:08 -0500

    feat: support mobile better

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,15 @@ import heroImage from '@/assets/home/hero.png';
 export default function Home() {
   return (
     <div>
-      <div className="relative">
+      <div className="sm:relative">
         <Image
           src={heroImage}
           alt="Hero image description"
-          className="absolute h-[80vh] object-cover"
+          className="h-[80vh] object-cover hidden sm:absolute sm:block"
           placeholder="blur"
           unoptimized
         />
-        <div className="bg-sky-700/30 absolute ml-[10%] h-[80vh] w-[30vw]" />
+        <div className="bg-sky-700/30 h-[80vh] mx-auto w-10/12 sm:absolute sm:ml-[10%] sm:w-[30vw] sm:min-w-80" />
       </div>
     </div>
   );
