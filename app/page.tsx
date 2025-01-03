25e1806c7e7cf716d4c7619a7319af9260de83c3
Seamus Lowry <spl1owry3@gmail.com>
2025-01-02 20:08:01 -0500

    feat: json configuration for text on the banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,40 @@
 import Image from 'next/image';
 import heroImage from '@/assets/home/hero.png';
 import bannerImage from '@/assets/home/banner.png';
+import { readFile } from 'fs/promises';
+import json5 from 'json5';
 
-export default function Home() {
+interface ContentConfiguration {
+  hero?: {
+    alt?: string;
+  };
+  banner: {
+    alt?: string;
+    title?: string;
+    email?: string;
+    content?: string[];
+  };
+}
+
+export default async function Home() {
+  const {
+    hero: { alt: heroDescription = '' } = {},
+    banner: {
+      alt: bannerDescription = '',
+      title = '',
+      email = '',
+      content: bannerContent = [],
+    } = {},
+  } = json5.parse<ContentConfiguration>(
+    await readFile(`./public/home/content.json5`, 'utf-8'),
+  );
+
   return (
     <div>
       <div className="grid grid-cols-[auto_auto_auto] sm:grid-cols-[10%_auto_auto] min-h-fit h-[80vh] max-h-[56rem]">
         <Image
           src={heroImage}
-          alt="Hero image description"
+          alt={heroDescription}
           className="object-cover hidden h-full row-start-1 col-start-1 col-span-3 sm:block"
           placeholder="blur"
           unoptimized
@@ -17,23 +43,21 @@ export default function Home() {
           <div className="flex flex-col items-center">
             <Image
               src={bannerImage}
-              alt="Banner image description"
+              alt={bannerDescription}
               placeholder="blur"
               className="object-cover w-[80%] aspect-square rounded-full"
               unoptimized
             />
-            <p className="text-3xl pt-3">Siobhan Oca, PhD</p>
-            <a href="mailto:skr23@duke.edu" className="underline text-xl pt-3">
-              skr23@duke.edu
+            <p className="text-3xl pt-3">{title}</p>
+            <a href={`mailto:${email}`} className="underline text-xl pt-3">
+              {email}
             </a>
           </div>
-          <p className="text-xl text-center">
-            Assistant Professor of Mechanical Engineering and Materials Science
-            at Duke University
-          </p>
-          <p className="text-xl text-center">
-            Assistant Director of Masters in Robotics at Duke University
-          </p>
+          {bannerContent.map((p, index) => (
+            <p key={index} className="text-xl text-center">
+              {p}
+            </p>
+          ))}
         </div>
       </div>
     </div>
--- utils/metadata.ts
+++ utils/metadata.ts
@@ -7,10 +7,6 @@ type DefinedMetadata = 'home';
 export async function retrieveMetadata(
   page: DefinedMetadata,
 ): Promise<Metadata> {
-  console.log(
-    json5.parse(await readFile(`./public/${page}/metadata.json5`, 'utf-8')),
-  );
-
   return json5.parse(
     await readFile(`./public/${page}/metadata.json5`, 'utf-8'),
   );
