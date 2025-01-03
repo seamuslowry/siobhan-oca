e79aa3d50ecc61180efb0a056d46c5a999070b7b
Seamus Lowry <spl1owry3@gmail.com>
2025-01-03 16:48:49 -0500

    feat/home hero (#13)
    * docs: fix incorrect doc
    
    * feat: initial hero and banner
    
    * chore: output directory
    
    * chore: delete webp
    
    * feat: blur placeholder
    
    * chore: try with static and unoptimized
    
    * feat: play with color and sizing of banner
    
    * refactor: move hero image asset under an assets directory
    
    * fix: actually include the image
    
    * feat: support mobile better
    
    * feat: banner image and email
    
    * feat: slide in animation
    
    * fix: only slide in with hero
    
    * fix: fit
    
    * feat: smaller image and centered
    
    * feat: just a little rounded
    
    * feat: responsive banner
    
    * feat: crop banner image
    
    * fix: use auto to prevent jump
    
    * docs: document hero and banner images
    
    * fix: actually responsive using grid overlaying
    
    * chore: remove unused
    
    * refactor: json5 for matadata
    
    * feat: json configuration for text on the banner
    
    * fix: will width
    
    * fix: actual full width
    
    * docs: text content documentation
    
    * fix: use fr to prevent screen jump on slow connections
    
    * refactor: use yaml rather than json5
    
    * docs: document the yaml matching its structure
    
    * docs: remove duplicative
    
    * fix: wrap in main tag
    
    * chore: smaller diff

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      keyframes: {
+        slideInFromLeft: {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '25%': { transform: 'translateX(-100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        slideInFromLeft: 'slideInFromLeft 1.5s ease-out',
       },
     },
   },
--- utils/metadata.ts
+++ utils/metadata.ts
@@ -1,18 +1,12 @@
 import { readFile } from 'fs/promises';
+import { parse } from 'yaml';
 import type { Metadata } from 'next';
 
 type DefinedMetadata = 'home';
 
-const DEFINED_METADATA_KEYS = ['title', 'description'] as const;
-
 export async function retrieveMetadata(
   page: DefinedMetadata,
 ): Promise<Metadata> {
-  return DEFINED_METADATA_KEYS.reduce<Promise<Metadata>>(
-    async (acc, curr) => ({
-      ...(await acc),
-      [curr]: await readFile(`./public/${page}/metadata/${curr}.txt`, 'utf8'),
-    }),
-    Promise.resolve({}),
-  );
+  return parse(await readFile(`./public/${page}/content.yaml`, 'utf-8'))
+    .metadata;
 }
