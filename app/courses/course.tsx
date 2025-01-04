9cf6d7919e8be5780a6d4e3d22eff68ea3570b63
Seamus Lowry <spl1owry3@gmail.com>
2025-01-04 10:14:07 -0500

    feat: dynamic course images

--- app/courses/course.tsx
+++ app/courses/course.tsx
@@ -1,11 +1,25 @@
+import Image from 'next/image';
+
 export interface Course {
   name?: string;
+  image?: string;
 }
 
-export function Course({ course: { name = '' } }: { course: Course }) {
+export async function Course({
+  course: { name = '', image: imagePath },
+}: {
+  course: Course;
+}) {
+  const image = (await import(`@/assets/courses/${imagePath}`)).default;
+
   return (
-    <div className="grid grid-cols-[3fr_7fr] bg-limestone dark:bg-graphite rounded m-3">
-      <div />
+    <div className="grid grid-cols-[3fr_7fr] bg-limestone dark:bg-graphite rounded-lg overflow-hidden m-3 h-96">
+      <Image
+        src={image}
+        alt="test"
+        unoptimized
+        className="object-cover h-full"
+      />
       <div>
         <p>{name}</p>
       </div>
