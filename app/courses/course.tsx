bff0376e0555a651043a36622a4df17295ce38de
Seamus Lowry <spl1owry3@gmail.com>
2025-01-04 10:18:52 -0500

    fix: include alt text for images

--- app/courses/course.tsx
+++ app/courses/course.tsx
@@ -2,23 +2,26 @@ import Image from 'next/image';
 
 export interface Course {
   name?: string;
-  image?: string;
+  image?: {
+    src?: string;
+    alt?: string;
+  };
 }
 
 export async function Course({
-  course: { name = '', image: imagePath },
+  course: { name = '', image: { src = '', alt = '' } = {} },
 }: {
   course: Course;
 }) {
-  const image = (await import(`@/assets/courses/${imagePath}`)).default;
+  const image = (await import(`@/assets/courses/${src}`)).default;
 
   return (
     <div className="grid grid-cols-[3fr_7fr] bg-limestone dark:bg-graphite rounded-lg overflow-hidden m-3 h-96">
       <Image
         src={image}
-        alt="test"
-        unoptimized
+        alt={alt}
         className="object-cover h-full"
+        unoptimized
       />
       <div>
         <p>{name}</p>
