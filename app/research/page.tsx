23bc5ac535b52792fde7f1d6b1840f7924c62ede
Seamus Lowry <spl1owry3@gmail.com>
2025-01-13 19:01:08 -0500

    feat: initial accordion implementation

--- app/research/page.tsx
+++ app/research/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { retrieveMetadata } from '@/utils/metadata';
 import { retrieveData } from '@/utils/research';
+import Accordion from '@/components/accordion';
+import { Fragment } from 'react';
 
 export async function generateMetadata(): Promise<Metadata> {
   return retrieveMetadata('research');
@@ -13,7 +15,15 @@ export default async function Research() {
     <main>
       <div className="mx-[8%] my-10 flex flex-col gap-8">
         {topics.map(({ name }, i) => (
-          <p key={i}>{name}</p>
+          <Accordion key={i} summary={name}>
+            <Fragment key={i}>
+              {Array(10)
+                .fill(0)
+                .map((_, i) => (
+                  <p key={i}>test</p>
+                ))}
+            </Fragment>
+          </Accordion>
         ))}
       </div>
     </main>
---
+++ components/accordion.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import clsx from 'clsx';
+import { ReactNode, useCallback, useState } from 'react';
+
+export default function Accordion({
+  children,
+  summary,
+}: {
+  children: ReactNode;
+  summary: ReactNode;
+}) {
+  const [open, setOpen] = useState(false);
+
+  const toggle = useCallback(() => setOpen(o => !o), []);
+
+  return (
+    <div className="w-full">
+      <button className="w-full" onClick={toggle}>
+        {summary}
+      </button>
+      <div
+        className={clsx(
+          'grid transition-all duration-300 ease-in-out',
+          open ? 'grid-rows-[1fr]' : 'grid-rows-[0fr]',
+        )}
+      >
+        <div className="overflow-hidden">{children}</div>
+      </div>
+    </div>
+  );
+}
