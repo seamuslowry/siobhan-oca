74c9ec375b03d1ec80fab5c23e6ec57ae4950095
Seamus Lowry <spl1owry3@gmail.com>
2025-01-24 22:33:55 -0500

    chore: hide team

--- utils/routes.ts
+++ utils/routes.ts
@@ -7,7 +7,7 @@ const ROUTES: Route[] = [
   { text: 'The Oca Lab', href: '/research' },
   { text: 'Teaching', href: '/courses' },
   // { text: 'News', href: '/' },
-  { text: 'Team', href: '/' },
+  // { text: 'Team', href: '/' },
 ];
 
 export async function retrieveRoutes() {
