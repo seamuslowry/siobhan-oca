7631fa71078798059584fba1d7e5af51fc5c0b40
Seamus Lowry <spl1owry3@gmail.com>
2025-01-25 07:30:52 -0500

    fix: calculate time on the client

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { retrieveMetadata } from '@/utils/metadata';
 import { retrieveData } from '@/utils/team';
 import { TeamMemberLink } from '@/components/team-member-link';
-import { formatDistanceStrict } from 'date-fns';
+import { Duration } from '@/components/duration';
 
 export async function generateMetadata(): Promise<Metadata> {
   return retrieveMetadata('team');
@@ -21,9 +21,8 @@ export default async function Team() {
             <p>Start Date: {member.start.toDateString()}</p>
             <p>End Date: {member.end?.toDateString()}</p>
             <p>
-              {/* TODO: this should be client. so it's actually calculated dynamically */}
               Time:{' '}
-              {formatDistanceStrict(member.end ?? new Date(), member.start)}
+              <Duration earlierDate={member.start} laterDate={member.end} />
             </p>
             <p>Current: {member.current}</p>
           </div>
