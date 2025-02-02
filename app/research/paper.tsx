412adf6aca59465ffbd999655c90948fff5dbc9a
Seamus Lowry <spl1owry3@gmail.com>
2025-02-02 18:45:17 -0500

    fix/remove uneditable link (#56)
    * fix: remove
    
    * fix: remove underline from non-link

--- app/research/paper.tsx
+++ app/research/paper.tsx
@@ -1,23 +1,12 @@
 import { type Paper as PaperType } from '@/utils/research';
 import { Fragment } from 'react';
 import { TextContent } from '@/components/text-content';
-import Link from 'next/link';
 
 export async function Paper({ paper }: { paper: PaperType }) {
   const authors = await paper.getAuthors();
   return (
     <div className="px-10 py-3">
-      <Link
-        href="/courses/test.pdf"
-        target="_blank"
-        rel="noopener noreferrer"
-        prefetch={false}
-      >
-        <TextContent
-          value={paper.title}
-          desired={{ bold: true, underline: true }}
-        />
-      </Link>
+      <TextContent value={paper.title} desired={{ bold: true }} />
       {authors.map((author, i, arr) => (
         <Fragment key={i}>
           <TextContent
