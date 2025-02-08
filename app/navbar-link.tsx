3523abfaf0a65b02bbe67f828b07e01eab7b5bc1
Seamus Lowry <spl1owry3@gmail.com>
2025-02-08 12:03:26 -0500

    fix: use flex property

--- app/navbar-link.tsx
+++ app/navbar-link.tsx
@@ -5,7 +5,7 @@ export async function NavbarLink({ route }: { route: Route }) {
   return 'href' in route ? (
     <ButtonLink href={route.href}>{route.text}</ButtonLink>
   ) : (
-    <div className="flex flex-col justify-items-center gap-y-1">
+    <div className="flex flex-col items-center gap-y-1">
       <p className="text-sm">{route.text}</p>
       <div>
         {route.routes.map(innerRoute => (
