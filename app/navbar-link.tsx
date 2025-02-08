4c82c3c6c7ce6b6cfb56d133986527fb221cd12e
Seamus Lowry <spl1owry3@gmail.com>
2025-02-08 11:52:16 -0500

    refactor: try with flex

--- app/navbar-link.tsx
+++ app/navbar-link.tsx
@@ -5,15 +5,15 @@ export async function NavbarLink({ route }: { route: Route }) {
   return 'href' in route ? (
     <ButtonLink href={route.href}>{route.text}</ButtonLink>
   ) : (
-    <div className="grid grid-rows-[min-content_auto] justify-items-center gap-y-1">
-      <p className={`row-start-1 col-span-${route.routes.length} text-sm`}>
+    <div className="flex flex-col justify-items-center gap-y-1">
+      <p className="text-sm">
         {route.text}
       </p>
-      {route.routes.map(innerRoute => (
-        <span key={innerRoute.text} className="row-start-2">
-          <NavbarLink route={innerRoute} />
-        </span>
-      ))}
+      <div>
+        {route.routes.map(innerRoute => (
+          <NavbarLink key={innerRoute.text} route={innerRoute} />
+        ))}
+      </div>
     </div>
   );
 }
