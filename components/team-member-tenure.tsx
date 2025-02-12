b7be97c85849e3d8342c82e220bb17ee398fc78c
Seamus Lowry <spl1owry3@gmail.com>
2025-02-11 20:40:44 -0500

    chore: remove start and end dates

--- components/team-member-tenure.tsx
+++ components/team-member-tenure.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { Duration } from '@/components/duration';
-import { format, isPast, min } from 'date-fns';
-import { enUS } from 'date-fns/locale/en-US';
+// import { format, isPast, min } from 'date-fns';
+import { min } from 'date-fns';
+// import { enUS } from 'date-fns/locale/en-US';
 import { useEffect, useState } from 'react';
 
 export default function TeamMemberTenure({
@@ -13,20 +14,20 @@ export default function TeamMemberTenure({
   start: Date;
 }) {
   const [nonFutureEnd, setNonFutureEnd] = useState<Date | undefined>(end);
-  const [endDate, setEndDate] = useState<Date | undefined>(end);
+  //   const [endDate, setEndDate] = useState<Date | undefined>(end);
   useEffect(() => {
     const now = new Date();
     setNonFutureEnd(min([end, now].filter(d => !!d)));
   }, [end]);
 
-  useEffect(() => {
-    setEndDate(end && isPast(end) ? end : undefined);
-  }, [end]);
+  //   useEffect(() => {
+  //     setEndDate(end && isPast(end) ? end : undefined);
+  //   }, [end]);
 
   return (
     <p className="text-md font-bold">
-      {format(start, 'MMM yyyy', { locale: enUS })} -{' '}
-      {endDate ? format(endDate, 'MMM yyyy', { locale: enUS }) : 'Current'} /{' '}
+      {/* {format(start, 'MMM yyyy', { locale: enUS })} -{' '}
+      {endDate ? format(endDate, 'MMM yyyy', { locale: enUS }) : 'Current'} /{' '} */}
       <Duration earlierDate={start} laterDate={nonFutureEnd} />
     </p>
   );
