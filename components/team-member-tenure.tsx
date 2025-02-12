d885d02e007fdfc8ca89932e454a0d45a29a7438
Seamus Lowry <spl1owry3@gmail.com>
2025-02-11 20:39:57 -0500

    fix: readd duration

--- components/team-member-tenure.tsx
+++ components/team-member-tenure.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-// import { Duration } from '@/components/duration';
-// import { format, isPast, min } from 'date-fns';
-import { format, isPast } from 'date-fns';
+import { Duration } from '@/components/duration';
+import { format, isPast, min } from 'date-fns';
 import { enUS } from 'date-fns/locale/en-US';
 import { useEffect, useState } from 'react';
 
@@ -13,12 +12,12 @@ export default function TeamMemberTenure({
   end?: Date;
   start: Date;
 }) {
-  //   const [nonFutureEnd, setNonFutureEnd] = useState<Date | undefined>(end);
+  const [nonFutureEnd, setNonFutureEnd] = useState<Date | undefined>(end);
   const [endDate, setEndDate] = useState<Date | undefined>(end);
-  //   useEffect(() => {
-  //     const now = new Date();
-  //     setNonFutureEnd(min([end, now].filter(d => !!d)));
-  //   }, [end]);
+  useEffect(() => {
+    const now = new Date();
+    setNonFutureEnd(min([end, now].filter(d => !!d)));
+  }, [end]);
 
   useEffect(() => {
     setEndDate(end && isPast(end) ? end : undefined);
@@ -28,7 +27,7 @@ export default function TeamMemberTenure({
     <p className="text-md font-bold">
       {format(start, 'MMM yyyy', { locale: enUS })} -{' '}
       {endDate ? format(endDate, 'MMM yyyy', { locale: enUS }) : 'Current'} /{' '}
-      {/* <Duration earlierDate={start} laterDate={nonFutureEnd} /> */}
+      <Duration earlierDate={start} laterDate={nonFutureEnd} />
     </p>
   );
 }
