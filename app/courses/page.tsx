41c3cf3643cec453ca9fef2128ee1779a275bf3d
siobhanoca <55724169+siobhanoca@users.noreply.github.com>
2026-03-12 18:36:07 -0400

    Feat courses page summary (#135)
    * adding new members
    
    * fix csv file
    
    * fix
    
    * fix
    
    * "members update 2"
    
    * new team members
    
    * add more members
    
    * fix research to add topics and papers
    
    * comitmesg
    
    * evan changes
    
    * msc add
    
    * itty bitty edit
    
    * moving summary teaching
    
    * Render courses page summary from content YAML
    
    ---------
    
    Co-authored-by: Siobhan Rigby <siobhanrigby@MacBook-Air-111.local>

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -4,17 +4,22 @@ import type { Metadata } from 'next';
 import { retrieveMetadata } from '@/utils/metadata';
 import { retrieveData } from '@/utils/courses';
 import Divider from '@/components/divider';
+import { TextContent } from '@/components/text-content';
 
 export async function generateMetadata(): Promise<Metadata> {
   return retrieveMetadata('courses');
 }
 
 export default async function Courses() {
-  const { courses } = await retrieveData();
+  const { courses, summary } = await retrieveData();
 
   return (
     <main>
       <div className="mx-[8%] my-10 flex flex-col gap-8">
+        {summary.map((piece, index) => (
+          <TextContent key={index} value={piece} />
+        ))}
+        {!!summary.length && <Divider />}
         {courses.map((c, i) => (
           <Fragment key={i}>
             <Course course={c} className="mx-[2%]" />
