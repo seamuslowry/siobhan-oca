42ece590efa08d94f78f0cfa317e7a1255478eac
Seamus Lowry <spl1owry3@gmail.com>
2026-03-13 21:53:28 -0400

    fix: remove image and type

--- app/team/team-member.tsx
+++ app/team/team-member.tsx
@@ -4,23 +4,7 @@ import { ExternalLink } from '@/components/external-link';
 import { MemberDuration } from '@/components/member-duration';
 import { MemberEndDate } from '@/components/member-end-date';
 import Link from 'next/link';
-import { createAvatar } from '@dicebear/core';
-import * as thumbs from '@dicebear/thumbs';
-import Image from 'next/image';
-import startCase from 'lodash.startcase';
 import { format } from 'date-fns/format';
-
-async function getImage(slug: string) {
-  try {
-    // this import won't fail in local dev if the file doesn't exist
-    // the import only fails when actually building the project
-    return (await import(`@/assets/team/${slug}.png`)).default;
-  } catch {
-    return createAvatar(thumbs, {
-      seed: slug,
-    }).toDataUri();
-  }
-}
 
 export default async function TeamMember({
   member,
@@ -30,21 +14,9 @@ export default async function TeamMember({
   const coursework = await member.getCoursework();
   const topics = await member.getTopics();
 
-  const avatarImg = await getImage(member.slug);
-  const isGeneratedAvatar = typeof avatarImg === 'string';
-
   return (
     <section>
       <div className="grid grid-cols-[auto_1fr] gap-x-6 items-center mb-8">
-        <Image
-          src={avatarImg}
-          alt={member.name}
-          className="object-cover min-w-32 aspect-square rounded-full"
-          placeholder={isGeneratedAvatar ? 'empty' : 'blur'}
-          width={128}
-          height={128}
-          unoptimized
-        />
         <span>
           {member.link ? (
             <ExternalLink href={member.link} className="text-3xl">
@@ -61,7 +33,6 @@ export default async function TeamMember({
             <MemberEndDate date={member.end} /> /{' '}
             <MemberDuration start={member.start} end={member.end} />
           </p>
-          <p className="italic">{startCase(member.type)}</p>
         </span>
       </div>
       {member.summary && (
