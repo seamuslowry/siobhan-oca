c161e4d5c8337d8913d75b9f5db59c5fceb31bea
Siobhan Rigby Oca <rigby.siobhan@gmail.com>
2026-03-28 10:01:06 -0400

    Revert unintended team member page changes

--- app/team/team-member.tsx
+++ app/team/team-member.tsx
@@ -4,22 +4,7 @@ import { ExternalLink } from '@/components/external-link';
 import { MemberDuration } from '@/components/member-duration';
 import { MemberEndDate } from '@/components/member-end-date';
 import Link from 'next/link';
-import { createAvatar } from '@dicebear/core';
-import * as thumbs from '@dicebear/thumbs';
-import Image from 'next/image';
-import startCase from 'lodash.startcase';
 import { format } from 'date-fns/format';
-
-async function getImage(slug: string) {
-  try {
-    // This import only fails during builds when no portrait exists.
-    return (await import(`@/assets/team/${slug}.png`)).default;
-  } catch {
-    return createAvatar(thumbs, {
-      seed: slug,
-    }).toDataUri();
-  }
-}
 
 export default async function TeamMember({
   member,
@@ -29,51 +14,36 @@ export default async function TeamMember({
   const coursework = await member.getCoursework();
   const topics = await member.getTopics();
 
-  const avatarImg = await getImage(member.slug);
-  const isGeneratedAvatar = typeof avatarImg === 'string';
-
   return (
     <section>
-      <div className="grid grid-cols-[auto_1fr] gap-x-6 items-center mb-8">
-        <Image
-          src={avatarImg}
-          alt={member.name}
-          className="object-cover min-w-32 aspect-square rounded-full"
-          placeholder={isGeneratedAvatar ? 'empty' : 'blur'}
-          width={128}
-          height={128}
-          unoptimized
-        />
+      <div className="grid grid-cols-[auto_1fr] gap-x-6 items-center mb-2">
         <span>
           {member.link ? (
-            <ExternalLink href={member.link} className="text-3xl">
+            <ExternalLink href={member.link} className="text-2xl">
               <TextContent
                 value={member.name}
-                desired={{ size: '3xl', underline: true }}
+                desired={{ size: '2xl', underline: true }}
               />
             </ExternalLink>
           ) : (
-            <TextContent value={member.name} desired={{ size: '3xl' }} />
+            <TextContent value={member.name} desired={{ size: '2xl' }} />
           )}
-          <p className="text-md font-bold">
+          <p className="text-md font-medium mt-1">
             {format(member.start, 'MMM yyyy')} -{' '}
             <MemberEndDate date={member.end} /> /{' '}
             <MemberDuration start={member.start} end={member.end} />
           </p>
-          <p className="italic">{startCase(member.type)}</p>
         </span>
       </div>
-      {member.summary && (
-        <p className="my-5 text-lg font-medium">{member.summary}</p>
-      )}
-      <div className="mx-4 flex flex-col gap-8">
+      {member.summary && <p className="pl-2 my-3 text-md">{member.summary}</p>}
+      <div className="mx-2 flex flex-col gap-4">
         {topics.length > 0 &&
           topics.map(t => (
             <div key={t.id}>
               <Link href={`/research#${t.id}`}>
                 <TextContent
                   value={t.name}
-                  desired={{ size: '2xl', underline: true }}
+                  desired={{ size: 'lg', underline: true }}
                 />
               </Link>
               <ul>
@@ -90,7 +60,7 @@ export default async function TeamMember({
             <div key={c.id}>
               <TextContent
                 value={c.name}
-                desired={{ size: '2xl', italic: true }}
+                desired={{ size: 'lg', italic: true }}
               />
               <ul>
                 {c.projects.map(p => (
